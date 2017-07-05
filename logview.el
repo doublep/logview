134b41557ab539219d9e3a1b3c8939df93676726
Paul Pogonyshev <pogonyshev@gmail.com>
2017-07-05 21:17:35 +0200

    Bump version because of an important bugfix.

--- logview.el
+++ logview.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.8
+;; Version:    0.8.1
 ;; Keywords:   files, tools
 ;; Homepage:   https://github.com/doublep/logview
 ;; Package-Requires: ((emacs "24.1") (datetime "0.2"))
