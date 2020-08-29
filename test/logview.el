d442857cf722e152adb5ae554af46efc886273ca
Paul Pogonyshev <pogonyshev@gmail.com>
2020-08-29 17:13:11 +0200

    Update copyright years.

--- test/logview.el
+++ test/logview.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
-;; Copyright (C) 2018 Paul Pogonyshev
+;; Copyright (C) 2018-2020 Paul Pogonyshev
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
