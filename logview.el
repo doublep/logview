32d833559100da489896e8791c9c6284ccb32ea5
Paul Pogonyshev <pogonyshev@gmail.com>
2023-07-30 18:41:32 +0200

    Post-release version bump.

--- logview.el
+++ logview.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.16.4
+;; Version:    0.16.5snapshot
 ;; Keywords:   files, tools
 ;; Homepage:   https://github.com/doublep/logview
 ;; Package-Requires: ((emacs "25.1") (datetime "0.8") (extmap "1.0"))
