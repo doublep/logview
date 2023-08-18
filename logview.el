a0f0ad3e0145eab382ff325cc95ec8ccbd6d68e3
Paul Pogonyshev <pogonyshev@gmail.com>
2023-08-18 16:11:38 +0200

    Release logview 0.17

--- logview.el
+++ logview.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.16.5snapshot
+;; Version:    0.17
 ;; Keywords:   files, tools
 ;; Homepage:   https://github.com/doublep/logview
 ;; Package-Requires: ((emacs "25.1") (datetime "0.8") (extmap "1.0"))
